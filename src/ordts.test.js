8aab1dcc0b94f740a89520a8c1dd3fb0960464b5
Aaron Sikes <aaron@sikes.io>
2019-05-29 10:45:52 -0400

    Add evaluation of delete atoms

--- src/ordts.test.js
+++ src/ordts.test.js
@@ -12,8 +12,20 @@ function id(index) {
 it('evaluates a basic string of atoms', function() {
   let atoms = [
     {type: 'root', id: id(0)},
-    {type: 'insert', value: 'h', id: id(1)},
-    {type: 'insert', value: 'i', id: id(2)},
+    {type: 'insert', id: id(1), value: {ch:'h', after: id(0)}},
+    {type: 'insert', id: id(2), value: {ch:'i', after: id(1)}},
   ];
   expect(evaluate(atoms)).toEqual('hi');
+});
+
+
+it('evaluates a string with deletions', function() {
+  let atoms = [
+    {type: 'root', id: id(0)},
+    {type: 'insert', id: id(1), value: {ch:'h', after: id(0)}},
+    {type: 'insert', id: id(2), value: {ch:'i', after: id(1)}},
+    {type: 'delete', id: id(3), value: id(2)},
+    {type: 'insert', id: id(4), value: {ch:'o', after: id(2)}},
+  ];
+  expect(evaluate(atoms)).toEqual('ho');
 });
--- src/ordts/sequence.js
+++ src/ordts/sequence.js
@@ -2,7 +2,9 @@ export function evaluate(collection) {
   let result = [];
   for (let atom of collection) {
     if (atom.type === 'insert') {
-      result.push(atom.value);
+      result.push(atom.value.ch);
+    } else if (atom.type === 'delete') {
+      result.pop();
     }
   }
   return result.join('');
