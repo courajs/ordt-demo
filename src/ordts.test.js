041de7309f555d63306f5e3bc67ca0ce0dcced9d
Aaron Sikes <aaron@sikes.io>
2019-05-30 20:11:36 -0400

    Fuck yeah, figure out new operations based on string diff

--- src/ordts.test.js
+++ src/ordts.test.js
@@ -1,81 +1,122 @@
-import { evaluate, indexedEvaluate, inferDiff } from './ordts/sequence.js';
+import { Sequence } from './ordts/sequence.js';
 
+const site = "0.6593416794305285";
 function id(index) {
   return {
-    site: "0.6593416794305285",
+    site,
     index: index,
     lamport: index,
-    wall: 1558986047518,
   };
 }
 
 describe('sequence ORDT evaluation', function() {
   it('evaluates a basic string of atoms', function() {
-    let atoms = [
+    let s = new Sequence(site, [
       {type: 'root', id: id(0)},
       {type: 'insert', id: id(1), value: {ch:'h', after: id(0)}},
       {type: 'insert', id: id(2), value: {ch:'i', after: id(1)}},
-    ];
-    expect(evaluate(atoms)).toEqual('hi');
+    ]);
+    expect(s.evaluate()).toEqual('hi');
   });
 
 
   it('evaluates a string with deletions', function() {
-    let atoms = [
+    let s = new Sequence(site, [
       {type: 'root', id: id(0)},
       {type: 'insert', id: id(1), value: {ch:'h', after: id(0)}},
       {type: 'insert', id: id(2), value: {ch:'i', after: id(1)}},
       {type: 'delete', id: id(3), value: id(2)},
       {type: 'insert', id: id(4), value: {ch:'o', after: id(2)}},
-    ];
-    expect(evaluate(atoms)).toEqual('ho');
+    ]);
+    expect(s.evaluate()).toEqual('ho');
   });
 
   it('can provide an index for the id of each character', function() {
-    let atoms = [
+    let s = new Sequence(site, [
       {type: 'root', id: id(0)},
       {type: 'insert', id: id(1), value: {ch:'h', after: id(0)}},
       {type: 'insert', id: id(2), value: {ch:'i', after: id(1)}},
       {type: 'delete', id: id(3), value: id(2)},
       {type: 'insert', id: id(4), value: {ch:'o', after: id(2)}},
-    ];
-    expect(indexedEvaluate(atoms)).toEqual({
+    ]);
+    expect(s.indexedEvaluate()).toMatchObject({
       value: 'ho',
       index: [id(1), id(4)],
     });
   });
 });
 
-describe('string new atom calculation', function() {
+describe('mutation operations', function() {
   it('detects if no change', function() {
     let atoms = [
       {type: 'root', id: id(0)},
       {type: 'insert', id: id(1), value: {ch:'a', after: id(0)}},
+      {type: 'insert', id: id(2), value: {ch:'b', after: id(1)}},
+      {type: 'delete', id: id(4), value: id(2)},
+      {type: 'insert', id: id(3), value: {ch:'c', after: id(2)}},
     ];
-    expect(inferDiff(atoms, 'a')).toEqual({
-      full: atoms,
-      fresh: [],
-    });
+    let s = new Sequence(site, atoms);
+    expect(s.become('ac')).toMatchObject([]);
+    expect(s.atoms).toMatchObject(atoms);
   });
 
-  xit('can infer deletion operations', function(){
+
+  it('can append to end', function() {
     let atoms = [
       {type: 'root', id: id(0)},
       {type: 'insert', id: id(1), value: {ch:'a', after: id(0)}},
-      {type: 'insert', id: id(2), value: {ch:'b', after: id(1)}},
-      {type: 'insert', id: id(3), value: {ch:'c', after: id(2)}},
     ];
-    expect(inferDiff(atoms, 'ac')).toEqual({
-      full: [
+    let s = new Sequence(site, atoms);
+    expect(s.become('ab')).toMatchObject([
+        {type: 'insert', id: id(2), value: {ch:'b', after: id(1)}}
+    ]);
+    expect(s.atoms).toMatchObject([
+      {type: 'root', id: id(0)},
+      {type: 'insert', id: id(1), value: {ch:'a', after: id(0)}},
+      {type: 'insert', id: id(2), value: {ch:'b', after: id(1)}},
+    ]);
+  });
+
+  it('can replace longer within', function() {
+    let atoms = [
         {type: 'root', id: id(0)},
         {type: 'insert', id: id(1), value: {ch:'a', after: id(0)}},
         {type: 'insert', id: id(2), value: {ch:'b', after: id(1)}},
+        {type: 'insert', id: id(3), value: {ch:'c', after: id(2)}},
+    ];
+    let s = new Sequence(site, atoms);
+    expect(s.become('axyc')).toMatchObject([
+        {type: 'delete', id: id(4), value: id(2)},
+        {type: 'insert', id: id(5), value: {ch:'x', after: id(1)}},
+        {type: 'insert', id: id(6), value: {ch:'y', after: id(5)}},
+    ]);
+    expect(s.atoms).toMatchObject([
+        {type: 'root', id: id(0)},
+        {type: 'insert', id: id(1), value: {ch:'a', after: id(0)}},
+        {type: 'insert', id: id(5), value: {ch:'x', after: id(1)}},
+        {type: 'insert', id: id(6), value: {ch:'y', after: id(5)}},
+        {type: 'insert', id: id(2), value: {ch:'b', after: id(1)}},
         {type: 'delete', id: id(4), value: id(2)},
         {type: 'insert', id: id(3), value: {ch:'c', after: id(2)}},
-      ],
-      fresh: [
-        {type: 'delete', id: id(4), value: id(2)},
-      ]
-    });
+    ]);
+  });
+
+  it('acceptance test', function() {
+    let cases = [
+      ['world', 'hello world', 6],
+      ['hello world', 'world', 6],
+      ['hello', 'hello world', 6],
+      ['hello world', 'hello', 6],
+      ['abcdef', 'abxyzef', 5],
+      ['abcdef', 'abxef', 3],
+    ];
+
+    for (let [start, target, diff] of cases) {
+      let s = new Sequence(site, [{type: 'root', id: id(0)}]);
+      s.become(start);
+      expect(s.evaluate()).toEqual(start);
+      expect(s.become(target).length).toEqual(diff);
+      expect(s.evaluate()).toEqual(target);
+    }
   });
 });
--- src/ordts/sequence.js
+++ src/ordts/sequence.js
@@ -1,39 +1,151 @@
-export function evaluate(collection) {
-  return indexedEvaluate(collection).value;
+export function idEq(a, b) {
+  return a.site === b.site && a.index === b.index;
 }
 
-export function indexedEvaluate(collection) {
-  let result = [];
-  for (let atom of collection) {
-    if (atom.type === 'insert') {
-      result.push(atom);
-    } else if (atom.type === 'delete') {
-      result.pop();
+export class Sequence {
+  constructor(id, atoms) {
+    this.id = id;
+    this.atoms = atoms;
+    this._determineIndexAndLamport();
+  }
+
+  _determineIndexAndLamport() {
+    let index = 0;
+    let lamport = 0;
+    for (let a of this.atoms) {
+      if (a.id.lamport > lamport) {
+        lamport = a.id.lamport;
+      }
+      if (a.id.site === this.id && a.id.index > index) {
+        index = a.id.index;
+      }
     }
+    this.currentIndex = index;
+    this.currentLamport = lamport;
   }
 
-  return {
-    value: result.map(a=>a.value.ch).join(''),
-    index: result.map(a=>a.id),
-  }
-}
-
-export function inferDiff(collection, str) {
-  let { value, index } = indexedEvaluate(collection);
-
-  let lastSame = 0;
-  for (let i=0; i<value.length && i<str.length; i++) {
-    if (value[i] == str[i]) {
-      lastSame = i;
-    } else {
-      break;
-    }
-  }
-
-  if (lastSame === value.length-1 && lastSame === str.length-1) {
+  nextId() {
     return {
-      full: collection.slice(),
-      fresh: [],
+      site: this.id,
+      index: ++this.currentIndex,
+      lamport: ++this.currentLamport,
+      wall: new Date().valueOf(),
+    };
+  }
+
+  evaluate() {
+    return this.indexedEvaluate().value;
+  }
+
+  indexedEvaluate() {
+    let result = [];
+    for (let atom of this.atoms) {
+      if (atom.type === 'insert') {
+        result.push(atom);
+      } else if (atom.type === 'delete') {
+        result.pop();
+      }
     }
+
+    return {
+      value: result.map(a=>a.value.ch).join(''),
+      index: result.map(a=>a.id),
+    }
+  }
+
+  insertAtom(atom) {
+    if (atom.id.site !== this.id) {
+      throw new Error('atom merge is not yet implemented for remote atoms');
+    }
+
+    if (atom.type === 'delete') {
+      let i = this.atoms.findIndex(a => idEq(a.id, atom.value));
+      if (i === -1) { return false; }
+      this.atoms.splice(i+1, 0, atom);
+      return true;
+    } else if (atom.type === 'insert') {
+      let i = this.atoms.findIndex(a => idEq(a.id, atom.value.after));
+      if (i === -1) { return false; }
+      while (this.atoms[i+1] && this.atoms[i+1].type === 'delete') { 
+        i++;
+      }
+      this.atoms.splice(i+1, 0, atom);
+      return true;
+    }
+  }
+
+  // Because of the way DOM input events work, we don't get granular information
+  // about exactly how an input/textarea is being changed. Instead, we have to
+  // figure it out based on the old and new values.
+  //
+  // This assumes that any edits have to be on a contiguous range - simultaneous
+  // edits on separate parts of the string shouldn't be possible in html inputs.
+  //
+  // Determine a diffRange - which range of characters should be removed,
+  // and where should the new ones be placed.
+  //
+  // range starts at the front edge of `start` character, ends at front edge of
+  // `end` character. So start=0,end=0, will insert at start of string.
+  // start=len-1,end=len-1 will insert at end of string.
+  // start=2,end=4, chars='hey' will delete 2nd & 3rd characters, and insert the
+  // 3-long string 'hey' in that slot.
+  //
+  // start=3, end=5, chars='xyz'
+  // 'ab cd ef'
+  // 'ab xyz ef'
+  //
+  // start=0, end=0, chars='hello '
+  // 'world'
+  // 'hello world'
+  //
+  // start=5, end=5, chars=' world'
+  // 'hello'
+  // 'hello world'
+  become(str) {
+    let { value, index } = this.indexedEvaluate();
+
+    let start = 0;
+    while (start < value.length && start < str.length && value[start] === str[start]) {
+      start++;
+    }
+
+    let back = 0;
+    while (
+      value.length - back > start &&
+      str.length - back > start &&
+      value[value.length-1 - back] === str[str.length-1 - back]
+    ) {
+      back++;
+    }
+
+    let fresh = [];
+
+    for (let i = start; i < value.length - back; i++) {
+      let atom = {
+        type: 'delete',
+        id: this.nextId(),
+        value: index[i],
+      }
+      fresh.push(atom);
+      this.insertAtom(atom);
+    }
+
+    let prevId = index[start-1] || this.atoms[0].id;
+    for (let i = start; i < str.length - back; i++) {
+      let id = this.nextId();
+      let atom = {
+        type: 'insert',
+        id: id,
+        value: {
+          after: prevId,
+          ch: str[i],
+        },
+      };
+      prevId = id;
+      fresh.push(atom);
+      this.insertAtom(atom);
+    }
+
+    return fresh;
   }
 }
